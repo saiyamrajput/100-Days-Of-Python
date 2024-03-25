ced075b907fcfbaf950fa01ec12b719f72875ff5
Saiyam Rajput <saiyamrajput28@gmail.com>
2024-03-25 19:15:01 -0400

    Add Day 1 Project

--- Day 1/Band_Name_Generator.py
+++ Day 1/Band_Name_Generator.py
@@ -1,5 +1,5 @@
-print("Welcome to the Band Name Generator.")
-city = input("What's the name of the city you grew up in?\n")
-pet = input("What's your pet's name?\n")
+print("Hello! Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
 combined_name = f"{city} {pet}"
 print("Your band name could be " + combined_name)
