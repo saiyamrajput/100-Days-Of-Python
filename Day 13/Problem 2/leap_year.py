6d328d87b2c37124bd49689b6bf4c3e3022fe068
Saiyam Rajput <saiyamrajput28@gmail.com>
2024-04-27 16:00:49 -0400

    update leap_year

--- Day 13/Problem 2/leap_year.py
+++ Day 13/Problem 2/leap_year.py
@@ -1,3 +1,20 @@
+'''
+year = input("Enter the year to check?")
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+'''
+
+#   Fixed Code
+
 year = int(input("Enter the year to check?"))
 
 if year % 4 == 0:
