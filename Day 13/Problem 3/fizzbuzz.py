3176aea08d31e0f72ffbffac383639421ec2396f
Saiyam Rajput <saiyamrajput28@gmail.com>
2024-04-27 16:03:15 -0400

    update fizzbuzz

--- Day 13/Problem 3/fizzbuzz.py
+++ Day 13/Problem 3/fizzbuzz.py
@@ -1,5 +1,19 @@
+'''
 for num in range(1, 101):
-  if num % 3 == 0 and v % 5 == 0:
+  if num % 3 == 0 or num % 5 == 0:
+    print("FizzBuzz")
+  if num % 3 == 0:
+    print("Fizz")
+  if num % 5 == 0:
+    print("Buzz")
+  else:
+    print([num])
+'''
+
+#   Fixed Code
+
+for num in range(1, 101):
+  if num % 3 == 0 and num % 5 == 0:
     print("FizzBuzz")
   elif num % 3 == 0:
     print("Fizz")
