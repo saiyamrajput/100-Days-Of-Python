7ea08b5e52e2e887254f9e44c696fc1be08d2993
Saiyam Rajput <saiyamrajput28@gmail.com>
2024-04-27 15:57:24 -0400

    update even_number

--- Day 13/Problem 1/even_number.py
+++ Day 13/Problem 1/even_number.py
@@ -1,3 +1,14 @@
+'''
+num = int(input("Enter the number to check?"))
+
+if num % 2 = 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+'''
+
+# Fixed code
+
 num = int(input("Enter the number to check?"))
 
 if num % 2 == 0:
