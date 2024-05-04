996c370872accc27d325f65194fa2d3fca12cd87
Saiyam Rajput <saiyamrajput28@gmail.com>
2024-05-04 04:14:43 -0400

    add quiz data

---
+++ Day 17/data.py
@@ -0,0 +1,159 @@
+question_data = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The HTML5 standard was published in 2014.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first computer bug was formed by faulty wires.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "All program codes have to be compiled into an executable "
+                    "file in order to be run. This file can then be executed on"
+                    " any machine.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The programming language 'Python' is based off a modified"
+                    " version of 'JavaScript'",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "AMD created the first consumer 64-bit processor.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "'HTML' stands for Hypertext Markup Language.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In most programming languages, the operator ++ is"
+                    " equivalent to the statement '+= 1'.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "The IBM PC used an Intel 8008 microprocessor clocked at"
+                    " 4.77 MHz and 8 kilobytes of memory.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "question": "A slug's blood is green.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "The loudest animal is the African Elephant.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "Approximately one quarter of human bones are in the feet.",
+        "correct_answer": "True"},
+    {
+        "question": "The total surface area of a human lungs is the size of a"
+                    " football pitch.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "In West Virginia, USA, if you accidentally hit an animal"
+                    " with your car, you are free to take it home "
+                    "to eat.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "In London, UK, if you happen to die in the House of"
+                    "Parliament, you are entitled to a state funeral.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "It is illegal to pee in the Ocean in Portugal.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "You can lead a cow down stairs but not up stairs.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "Google was originally called 'Backrub'.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "Buzz Aldrin's mother's maiden name was 'Moon'.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "No piece of square dry paper can be folded in half more"
+                    " than 7 times.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "A few ounces of chocolate can to kill a small dog.",
+        "correct_answer": "True"
+    }
+]
