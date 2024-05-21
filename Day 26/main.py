deb8945ee1a840e3a8ec2229b2e0806d05601111
Saiyam Rajput <saiyamrajput28@gmail.com>
2024-05-20 22:21:05 -0400

    update Day 26 Project

--- Day 26/main.py
+++ Day 26/main.py
@@ -7,11 +7,22 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # creating dictionary
 data_dic = {code.letter: code.code for (index, code) in data.iterrows()}
 
-# asking user for the input
-word = input("Enter a word: ").upper()
 
-# creating NATO alphabet
-words = [data_dic[alphabet] for alphabet in word]
+def generate():
+    """Generates Phonetic words"""
+    # asking user for the input
+    word = input("Enter a word: ").upper()
 
-# printing the words
-print(words)
+    # handling error
+    try:
+        # creating NATO alphabet
+        words = [data_dic[alphabet] for alphabet in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate()
+    else:
+        # printing the words
+        print(words)
+
+
+generate()
