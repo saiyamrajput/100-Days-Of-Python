17d534d9517a2bdf8436ab5acd0d4c5e04fcd9f8
Saiyam Rajput <saiyamrajput28@gmail.com>
2024-06-05 18:20:18 -0400

    add Day 55 Project

---
+++ Day 55/main.py
@@ -0,0 +1,52 @@
+# --------------------------- CHALLENGE -------------------------------------- #
+# def make_bold(function):
+#     def wrapper():
+#         return "<b>" + function() + "</b>"
+#     return wrapper
+#
+#
+# def make_emphasis(function):
+#     def wrapper():
+#         return "<em>" + function() + "</em>"
+#     return wrapper
+#
+#
+# def make_underlined(function):
+#     def wrapper():
+#         return "<u>" + function() + "</u>"
+#     return wrapper
+# ---------------------------------------------------------------------------- #
+
+# import statements
+from flask import Flask
+import random
+
+# generating random number
+random_number = random.randint(0, 9)
+
+# running the web page
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "<h1>Guess a number between 0 and 9</h1>" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+
+
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > random_number:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+
+    elif guess < random_number:
+        return "<h1 style='color: red'>Too low, try again!</h1>"\
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
